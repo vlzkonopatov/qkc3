3d858a975c0e4953d6759fc3173b5ec02255be5a
dakonopatov <dmitriy.konopatov@migogroup.ru>
2018-10-27 09:35:29 +0300

    update package.json and delete test example

--- test/web3.extend.js
+++ test/web3.extend.js
@@ -1,52 +1,52 @@
 var chai = require('chai');
 var assert = chai.assert;
 var FakeHttpProvider = require('./helpers/FakeHttpProvider');
-var Web3 = require('../lib/web3');
-var web3 = new Web3();
+var Qkc3 = require('../lib/Qkc3');
+var Qkc3 = new Qkc3();
 
 
 var tests = [{
-    properties: [new web3._extend.Property({
+    properties: [new Qkc3._extend.Property({
         name: 'gasPrice',
         getter: 'eth_gasPrice',
-        outputFormatter: web3._extend.formatters.outputBigNumberFormatter
+        outputFormatter: Qkc3._extend.formatters.outputBigNumberFormatter
     })]
 },{
-    methods: [new web3._extend.Method({
+    methods: [new Qkc3._extend.Method({
         name: 'getBalance',
         call: 'eth_getBalance',
         params: 2,
-        inputFormatter: [web3._extend.utils.toAddress, web3._extend.formatters.inputDefaultBlockNumberFormatter],
-        outputFormatter: web3._extend.formatters.outputBigNumberFormatter
+        inputFormatter: [Qkc3._extend.utils.toAddress, Qkc3._extend.formatters.inputDefaultBlockNumberFormatter],
+        outputFormatter: Qkc3._extend.formatters.outputBigNumberFormatter
     })]
 },{
     property: 'admin',
-    properties: [new web3._extend.Property({
+    properties: [new Qkc3._extend.Property({
         name: 'gasPrice',
         getter: 'eth_gasPrice',
-        outputFormatter: web3._extend.formatters.outputBigNumberFormatter
+        outputFormatter: Qkc3._extend.formatters.outputBigNumberFormatter
     })],
-    methods: [new web3._extend.Method({
+    methods: [new Qkc3._extend.Method({
         name: 'getBalance',
         call: 'eth_getBalance',
         params: 2,
-        inputFormatter: [web3._extend.utils.toAddress, web3._extend.formatters.inputDefaultBlockNumberFormatter],
-        outputFormatter: web3._extend.formatters.outputBigNumberFormatter
+        inputFormatter: [Qkc3._extend.utils.toAddress, Qkc3._extend.formatters.inputDefaultBlockNumberFormatter],
+        outputFormatter: Qkc3._extend.formatters.outputBigNumberFormatter
     })]
 }];
 
-describe('web3', function () {
+describe('Qkc3', function () {
     describe('_extend', function () {
         tests.forEach(function (test, index) {
             it('test no: ' + index, function () {
-                web3._extend(test);
+                Qkc3._extend(test);
 
 
                 if(test.properties)
                     test.properties.forEach(function(property){
 
                         var provider = new FakeHttpProvider();
-                        web3.setProvider(provider);
+                        Qkc3.setProvider(provider);
                         provider.injectResult('');
                         provider.injectValidation(function (payload) {
                             assert.equal(payload.jsonrpc, '2.0');
@@ -54,20 +54,20 @@ describe('web3', function () {
                         });
 
                         if(test.property) {
-                            assert.isObject(web3[test.property][property.name]);
-                            assert.isFunction(web3[test.property]['get'+ property.name.charAt(0).toUpperCase() + property.name.slice(1)]);
+                            assert.isObject(Qkc3[test.property][property.name]);
+                            assert.isFunction(Qkc3[test.property]['get'+ property.name.charAt(0).toUpperCase() + property.name.slice(1)]);
                         } else {
-                            assert.isObject(web3[property.name]);
-                            assert.isFunction(web3['get'+ property.name.charAt(0).toUpperCase() + property.name.slice(1)]);                        
+                            assert.isObject(Qkc3[property.name]);
+                            assert.isFunction(Qkc3['get'+ property.name.charAt(0).toUpperCase() + property.name.slice(1)]);
                         }
                     });
 
                 if(test.methods)
                     test.methods.forEach(function(property){
                         if(test.property)
-                            assert.isFunction(web3[test.property][property.name]);
+                            assert.isFunction(Qkc3[test.property][property.name]);
                         else
-                            assert.isFunction(web3[property.name]);
+                            assert.isFunction(Qkc3[property.name]);
                     });
 
             });
